df3b29e8f012ce1eb6808d4a1585cf806cfd3172
rmark-0201A07CD <dev.markwick@gmail.com>
2015-08-28 11:37:09 -0400

    Initial Commit

---
+++ DocumentKit/DocumentKit.h
@@ -0,0 +1,19 @@
+//
+//  DocumentKit.h
+//  DocumentKit
+//
+//  Created by Robbie Markwick on 8/28/15.
+//  Copyright © 2015 Robbie Markwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DocumentKit.
+FOUNDATION_EXPORT double DocumentKitVersionNumber;
+
+//! Project version string for DocumentKit.
+FOUNDATION_EXPORT const unsigned char DocumentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentKit/PublicHeader.h>
+
+
